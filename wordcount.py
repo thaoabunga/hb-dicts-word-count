233ee8480373fd62be16d5b95b5ab27422b69aba
thao le <thaoabunga@users.noreply.github.com>
2016-10-03 13:55:03 -0700

    Add files via upload
    broke down code into 3 different functions

--- wordcount.py
+++ wordcount.py
@@ -1,15 +1,48 @@
 # put your code here.
-my_file = open('test.txt')
 
-counting_words = {}
-
-def count():
+def tokenize(file_path):
+    """return a list of words from the file"""
+    tokens = []
+    my_file = open(file_path)
     for line in my_file:
         line = line.strip().split()
-        for word in line:
+        tokens.extend(line)
+    print tokens
+
+tokenize('test.txt')
+
+def count():
+
+    counting_words = {}
+
+    for word, value in counting_words.items():
             counting_words[word] = counting_words.get(word,0) + 1
+            
     print counting_words
 
-    my_file.close()
+    # print 'word = %r, value = %r' % (word, value)
 
-count()
+# def text(word):
+#     # return counting_words.get(word, 0)
+#     print counting_words
+
+# count()
+
+# # #1. function1: open the my_file
+# # #2 function2: create dictionary
+# # #3 function3: print the keys and values of the dictionary
+
+# my_file = open('test.txt')
+
+# counting_words = {}
+
+# def count():
+#     for line in my_file:
+#         line = line.strip().split()
+#         for word in line:
+#             counting_words[word] = counting_words.get(word,0) + 1
+#     print counting_words
+
+#     my_file.close()
+
+# count()
